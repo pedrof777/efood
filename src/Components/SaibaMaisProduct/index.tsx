e350df9b926f117ac2a6c3f69973936e70da5de8
unknown <pedrohenriquef278@gmail.com>
2025-11-12 18:55:37 -0300

    feat: Modal adicionado

--- src/Components/SaibaMaisProduct/index.tsx
+++ src/Components/SaibaMaisProduct/index.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react'
+
 import {
   Button,
+  ButtonModal,
+  Close,
   ContainerImg,
   ContainerInfo,
   ContainerProduct,
   Description,
+  Modal,
+  ModalContent,
+  ModalImg,
+  ModalText,
   TitleInfo
 } from './styles'
+
+import marguerita from '../../assets/images/pizzaMarguerita.png'
+import close from '../../assets/images/close.png'
 
 type Props = {
   image: string
@@ -13,17 +24,57 @@ type Props = {
   description: string
 }
 
-const SaibaMaisProduct = ({ description, image, title }: Props) => (
-  <ContainerProduct>
-    <ContainerImg>
-      <img src={image} alt="pizza marguerita" />
-    </ContainerImg>
-    <ContainerInfo>
-      <TitleInfo>{title}</TitleInfo>
-      <Description>{description}</Description>
-      <Button>Adicionar ao carrinho</Button>
-    </ContainerInfo>
-  </ContainerProduct>
-)
+const SaibaMaisProduct = ({ description, image, title }: Props) => {
+  const [modalAberto, setModalAberto] = useState(false)
+
+  return (
+    <>
+      <ContainerProduct>
+        <ContainerImg>
+          <img src={image} alt="pizza marguerita" />
+        </ContainerImg>
+        <ContainerInfo>
+          <TitleInfo>{title}</TitleInfo>
+          <Description>{description}</Description>
+          <Button onClick={() => setModalAberto(true)}>
+            Adicionar ao carrinho
+          </Button>
+        </ContainerInfo>
+      </ContainerProduct>
+      {modalAberto && (
+        <Modal>
+          <ModalContent>
+            <Close onClick={() => setModalAberto(false)}>
+              <img src={close} alt="Botao de fechar" />
+            </Close>
+            <ModalImg>
+              <img src={marguerita} alt="pizza" />
+            </ModalImg>
+            <ModalText>
+              <p>Pizza Marguerita</p>
+              <p>
+                A pizza Margherita é uma pizza clássica da culinária italiana,
+                reconhecida por sua simplicidade e sabor inigualável. Ela é
+                feita com uma base de massa fina e crocante, coberta com molho
+                de tomate fresco, queijo mussarela de alta qualidade, manjericão
+                fresco e azeite de oliva extra-virgem. A combinação de sabores é
+                perfeita, com o molho de tomate suculento e ligeiramente ácido,
+                o queijo derretido e cremoso e as folhas de manjericão frescas,
+                que adicionam um toque de sabor herbáceo. É uma pizza simples,
+                mas deliciosa, que agrada a todos os paladares e é uma ótima
+                opção para qualquer ocasião.
+                <br />
+                <br />
+                Serve: de 2 a 3 pessoas
+              </p>
+              <ButtonModal>Adicionar ao carrinho - R$ 69,90</ButtonModal>
+            </ModalText>
+          </ModalContent>
+          <div onClick={() => setModalAberto(false)} className="overlay"></div>
+        </Modal>
+      )}
+    </>
+  )
+}
 
 export default SaibaMaisProduct
--- src/Components/SaibaMaisProduct/styles.ts
+++ src/Components/SaibaMaisProduct/styles.ts
@@ -41,3 +41,60 @@ export const Button = styled.button`
   text-align: center;
   padding: 4px 0;
 `
+export const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+`
+export const ModalContent = styled.div`
+  max-width: 1024px;
+  position: relative;
+  z-index: 1;
+  display: flex;
+  background-color: ${Cores.laranja};
+`
+export const ModalImg = styled.div`
+  margin: 32px 24px 32px 32px;
+
+  img {
+    width: 280px;
+    height: 280px;
+    object-fit: cover;
+  }
+`
+export const ModalText = styled.div`
+  color: #fff;
+  margin: 32px 32px 0 0;
+
+  p {
+    margin: 16px 0;
+    font-size: 14px;
+    line-height: 22px;
+  }
+`
+export const ButtonModal = styled(Button)`
+  padding: 4px 7px;
+  width: auto;
+`
+
+export const Close = styled.div`
+  position: relative;
+  left: 1000px;
+  top: 8px;
+  cursor: pointer;
+`
